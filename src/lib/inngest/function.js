c35cbd2ebec976cd5cb2c72e346f0234d188d340
suibari <404.not.found.address@gmail.com>
2024-09-23 18:44:59 +0900

    更新1時間以内ならレコード更新を行わないロジック追加

--- src/lib/inngest/function.js
+++ src/lib/inngest/function.js
@@ -47,28 +47,37 @@ export function getPostsLikesAndUpdateDbFunction(group) {
 
       console.log(`[INNGEST] RECORDS G${group}: Executing get posts and likes: ${handleCenter}`);
       
-      const {data, err} = await supabase.from('elements').select('elements').eq('handle', handleCenter);
+      let {data, error} = await supabase.from('elements').select('elements').eq('handle', handleCenter);
       
       if (data.length === 1) {
         const nodes = data[0].elements.filter(element => (element.group === 'nodes'));
         const endIndex = Math.min(ELEM_NUM_PER_GROUP*(group+1), nodes.length);
         for (let i = ELEM_NUM_PER_GROUP*group; i < endIndex; i++) {
           const handleAround = nodes[i].data.handle;
-          try {
-            const records = await agent.getLatestPostsAndLikes(handleAround);
 
-            // ポスト解析イベントを駆動: マルチスレッドで走らせないと60sに間に合わない
-            await inngest.send({ name: 'hirogaru/updateDb.analyzeRecords', data: { handle: handleAround, records } });
-
-          } catch (e) {
-            console.error(`[INNGEST] RECORDS G${group}: Failed to get posts and likes: ${handleAround}`, e);
-            return { success: false, error: e.message };
-          }
+          let {data, error} = await supabase.from('records').select('updated_at').eq('handle', handleAround);
+          const previousUpdate = data.length > 0 ? new Date(data[0].updated_at).getTime() : null;
+          const currentTime = new Date().getTime();
+          const timeDiff = previousUpdate ? (currentTime - previousUpdate) : Infinity;
+          
+          if (timeDiff > (60 * 60 * 1000) || data.length === 0) {
+            // 1時間以上経過している、またはレコードが存在しない場合
+            try {
+              const records = await agent.getLatestPostsAndLikes(handleAround);
+          
+              // ポスト解析イベントを駆動: マルチスレッドで走らせないと60sに間に合わない
+              await inngest.send({ name: 'hirogaru/updateDb.analyzeRecords', data: { handle: handleAround, records } });
+          
+            } catch (e) {
+              console.error(`[INNGEST] RECORDS G${group}: Failed to get posts and likes: ${handleAround}`, e);
+              return { success: false, error: e.message };
+            }
+          } // 更新1時間以内かつレコードも存在するなら何もしない
         }
-
         console.log(`[INNGEST] RECORDS G${group}: exec time was ${timeLogger.tac()} [sec]: ${handleCenter}`);
         return { success: true };
       } else {
+        // 相関図データがない場合
         console.warn(`[INNGEST] RECORDS G${group}: Cannot get elements from DB: ${handleCenter}`);
       }
     }
@@ -86,8 +95,10 @@ export const analyzeRecordsFunction = inngest.createFunction(
       handle: handle,
       records: null,
       result_analyze: result,
-      updated_at: new Date()
+      updated_at: new Date().toISOString(),
     }).select();
+    console.log(`[INNGEST] ANALYZE ${handle}: finished.`)
+
     if (error) console.error("Error", error);
   }
 );
