50b1d5920bd3f403a4bab97b95f7451e733766ae
suibari <404.not.found.address@gmail.com>
2024-09-28 19:48:21 +0900

    タブを初めて開いたときだけ更新リクエストを出すよう修正

--- src/lib/server/trend.js
+++ src/lib/server/trend.js
@@ -1,5 +1,6 @@
 import { err } from "inngest/types";
 import { getAllRows, getRandomRows, supabase } from "./supabase";
+import { prettyErrorSplitter } from "inngest/helpers/errors";
 
 const RECORDS_NUM = 1000;
 
@@ -7,53 +8,57 @@ export async function getTrend() {
   const trendsToday = [];
   const trendsIncRate = [];
 
-  const data = await getAllRows({
-    tableName: 'records',
-    rowQuery: 'result_analyze',
-  });
+  // const data = await getAllRows({
+  //   tableName: 'records',
+  //   rowQuery: 'result_analyze',
+  // });
+  const { data, error } = await supabase
+    .rpc('get_word_freq_map');
+  if (error) { 
+    console.error(error);
+    throw error;
+  }
 
   // 各レコードデータに対して、
   // result_analyze.wordFreqFullMapがあれば、それをマージしていく
   // マージ処理
-  data.forEach(item => {
-    if (item.result_analyze.wordFreqMap) {
-      const now = new Date(); // 現在時刻
-      const today = new Date(now.getTime() - 24 * 60 * 60 * 1000); // 24時間前
-      const yesterday = new Date(now.getTime() - 48 * 60 * 60 * 1000); // 48時間前
+  const now = new Date(); // 現在時刻
+  const today = new Date(now.getTime() - 24 * 60 * 60 * 1000); // 24時間前
+  const yesterday = new Date(now.getTime() - 48 * 60 * 60 * 1000); // 48時間前
 
-      item.result_analyze.wordFreqMap.forEach(word => {
-        const occurrences = word.occurrences;
+  data.forEach(row => {
+    row.word_freq_map.forEach(word => {
+      const occurrences = word.occurrences;
 
-        let countToday = 0;
-        occurrences.forEach(occurrence => {
-          const occurrenceTime = new Date(occurrence.timestamp);
-          if (occurrenceTime >= today && occurrenceTime <= now) {
-            countToday++;
-          };
-        });
-
-        let countYesterday = 0;
-        occurrences.forEach(occurrence => {
-          const occurrenceTime = new Date(occurrence.timestamp);
-          if (occurrenceTime >= yesterday && occurrenceTime < today) {
-            countYesterday++;
-          };
-        });
-
-        // 今日のトレンド
-        trendsToday.push({
-          noun: word.noun,
-          count: countToday,
-        });
-
-        // 昨日から今日にかけての増加率
-        const incRate = countToday / (countYesterday || 1);
-        trendsIncRate.push({
-          noun: word.noun,
-          count: incRate,
-        });
+      let countToday = 0;
+      occurrences.forEach(occurrence => {
+        const occurrenceTime = new Date(occurrence.timestamp);
+        if (occurrenceTime >= today && occurrenceTime <= now) {
+          countToday++;
+        };
       });
-    }
+
+      let countYesterday = 0;
+      occurrences.forEach(occurrence => {
+        const occurrenceTime = new Date(occurrence.timestamp);
+        if (occurrenceTime >= yesterday && occurrenceTime < today) {
+          countYesterday++;
+        };
+      });
+
+      // 今日のトレンド
+      trendsToday.push({
+        noun: word.noun,
+        count: countToday,
+      });
+
+      // 昨日から今日にかけての増加率
+      const incRate = countToday / (countYesterday || 1);
+      trendsIncRate.push({
+        noun: word.noun,
+        count: incRate,
+      });
+    });
   });
 
   // できたマージ結果をソートしDBに格納
