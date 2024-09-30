39fce10f07cca5879fd45e10d5b5ce079707d573
suibari <404.not.found.address@gmail.com>
2024-09-30 23:20:34 +0900

    OGP設定お試し

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/stores';
 </script>
+
+<svelte:head>
+	<meta property="og:image" content="{$page.url.origin}/ogp.png" />
+</svelte:head>
 
 <slot></slot>
